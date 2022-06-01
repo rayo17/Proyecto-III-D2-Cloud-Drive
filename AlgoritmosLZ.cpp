557bb72077602d4769dd1181e29715f7cc5aff48
Valerin Calderon <76504802+VKCB@users.noreply.github.com>
2022-06-01 14:38:15 -0600

    Se añade algoritmoo lz78 y lzw

--- AlgoritmosLZ.cpp
+++ AlgoritmosLZ.cpp
@@ -324,3 +324,208 @@ string LZ77(string input, int option)
 		return result;
 	}
 }
+
+string LZ78(string input, int option)
+{
+	if (option == 1)
+	{
+		Node *dictionary = new Node;
+		string word, result;
+		int length, last_seen, index = 1;
+
+		length = (int)input.length();
+		word = input[0];
+		st_Node(dictionary, 1, word);
+		result += "0," + word;
+
+		for (int i = 1; i < length; i++)
+		{
+			string data;
+			data = input[i];
+
+		re_check:
+			Node *search = search_Node(dictionary, data);
+
+			if (search)
+			{
+				i++;
+				data += input[i];
+				last_seen = search->index;
+				goto re_check;
+			}
+			else
+			{
+				char zero;
+				if (input[i] == ' ')
+					zero = '0';
+				else
+					zero = input[i];
+
+				if ((int)data.length() < 2)
+					result += " " + to_string(0) + "," + zero;
+				else
+					result += " " + to_string(last_seen) + "," + zero;
+
+				index++;
+				if (i != length)
+					insert_Node(dictionary, index, data);
+			}
+		}
+
+		return result;
+	}
+	if (option == 2)
+	{
+		Node *dictionary = new Node;
+		string result;
+
+		vector <string> s_input = split(input, ' ');
+		int zz = 2;
+		for (int i = 0; i < s_input.size(); i++)
+		{
+			vector <string> ss_input = split(s_input[i], ',');
+
+			if (i == 0)
+			{
+				st_Node(dictionary, 1, ss_input[1]);
+				result += ss_input[1];
+			}
+			else
+			{
+				Node *serched;
+				string get_search = ss_input[1];
+				serched = search_Node(dictionary, stoi(ss_input[0]));
+				if (serched)
+				{
+					result += serched->data + get_search;
+					get_search = serched->data + split(s_input[i], ',')[1];
+					insert_Node(dictionary, zz, get_search);
+				}
+				else
+				{
+					if (stoi(ss_input[0]) == 0)
+						insert_Node(dictionary, zz, get_search);
+					else
+						insert_Node(dictionary, zz, get_search);
+
+					result += get_search;
+				}
+				zz++;
+			}
+		}
+
+		if (result[(int)result.length() - 1] == '0')
+			result = result.substr(0, result.size() - 1);
+		
+		return result;
+	}
+}
+
+string LZW(string input, int option)
+{
+	if (option == 1)
+	{
+		Node *dictionary = new Node;
+		string result;
+		int length, last_seen, index = 128;
+
+		st_Node(dictionary, 32, " ");
+		for (int i = 33; i < 128; i++)
+		{
+			string data;
+			data = i;
+			insert_Node(dictionary, i, data);
+		}
+
+		length = (int)input.length();
+
+		for (int i = 0; i < length; i++)
+		{
+			Node *searched;
+			string search;
+			search = input[i];
+
+		re_search:
+			searched = search_Node(dictionary, search);
+			if (searched)
+			{
+				i++;
+				search += input[i];
+				last_seen = searched->index;
+				if (i != length)
+					goto re_search;
+				else
+					goto print;
+			}
+			else
+			{
+				insert_Node(dictionary, index, search);
+				index++;
+			print:
+				result += to_string(last_seen) + " ";
+				i--;
+			}
+		}
+
+		return result;
+	}
+	if (option == 2)
+	{
+		Node *dictionary = new Node;
+		string result;
+		int index = 128;
+
+		st_Node(dictionary, 32, " ");
+		for (int i = 33; i < 128; i++)
+		{
+			string data;
+			data = i;
+			insert_Node(dictionary, i, data);
+		}
+
+		vector <string> s_input = split(input, ' ');
+		for (int i = 0; i < s_input.size(); i++)
+		{
+			Node *searched;
+			int search;
+			search = stoi(s_input[i]);
+
+			searched = search_Node(dictionary, search);
+
+			string cur, prev, data;
+			if (searched)
+				cur = search_Node(dictionary, stoi(s_input[i]))->data;
+			if (i != 0)
+				prev = search_Node(dictionary, stoi(s_input[i - 1]))->data;
+			else
+				prev = cur;
+
+			int show = 0;
+			if (searched)
+			{
+				result += searched->data;
+
+				if (i != 0)
+				{
+					data = prev + cur[0];
+					if (show != 1)
+					{
+						insert_Node(dictionary, index, data);
+						index++;
+					}
+				}
+				show = 0;
+			}
+			else
+			{
+				data = prev + prev[0];
+				insert_Node(dictionary, index, data);
+				index++;
+				show = 1;
+				result += data;
+			}
+		}
+
+		return result;
+	}
+}
