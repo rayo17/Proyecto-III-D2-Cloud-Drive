c7854cf9d1df97cb787e8da031e113acb95f41b9
Valerin Calderon <76504802+VKCB@users.noreply.github.com>
2022-06-08 19:51:50 -0600

    Revert "se crea la carpeta backend y se inicia un servidor y se estable un puerto"
    This reverts commit abf47c10dad0098a62a8dc4e2097854bc39b8a16.

--- backend/src/application.js
+++
@@ -1,6 +0,0 @@
-const express=require("express")
-const app=express(); //devuelve un objeto express este es nuestro serviodr
-
-
-
-module.exports=app
--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-
-const application=require("./application")
-async function main(Port){
-
-    await application.listen(Port);
-    console.log(`server connect on port ${Port}`);//puerto a la escucha
-
-}
-main(4000);
