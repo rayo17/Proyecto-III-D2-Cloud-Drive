47976ad1798e550678cf2c67be674e4169d74c74
Valerin Calderon <76504802+VKCB@users.noreply.github.com>
2022-06-08 19:51:45 -0600

    Revert "Estructurando la api"
    This reverts commit d5af8ce72a958a04419c501caa904359f8954efd.

--- backend/src/application.js
+++ backend/src/application.js
@@ -1,15 +1,6 @@
 const express=require("express")
-const cors=require("cors");
 const app=express(); //devuelve un objeto express este es nuestro serviodr
 
-//settings
-app.set("port",process.env.PORT||4000);//estableciendo el puerto
 
-// middlewares son procesos que se ejecutan intermedio entre el backend y el frontend
-app.use(cors());
-app.use(express.json());
 
-//routes rutas del servidor
-app.use("/appi/users",require("./routes/users"));
-
-module.exports=app;
+module.exports=app
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,9 @@
 
 const application=require("./application")
-const port=application.get("port") // se estable el puerto
 async function main(Port){
 
     await application.listen(Port);
     console.log(`server connect on port ${Port}`);//puerto a la escucha
 
 }
-main(port);
+main(4000);
