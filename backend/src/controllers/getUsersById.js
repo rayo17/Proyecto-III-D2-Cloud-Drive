01c6c5b14efa8ed1e90fbbd03cbb8aaaa33ceafe
Rayo <drayo.dard.16@gmail.com>
2022-06-15 12:25:22 -0600

    validacion del login desdesde el backend

--- backend/src/controllers/getUsersById.js
+++ backend/src/controllers/getUsersById.js
@@ -1,4 +1,23 @@
+const usuario=require("../models/usuario");
+const User=require("../models/usuario");
+
 const getUser=async (req,resp)=>{
+    const {userId}=req.params
+    if(IdleDeadline.length===15){
+        User.findBy(id).then(ususario=>{
+            if(!usuario){
+                return res.json({message: "no se encontro el usuario"});
+
+            }else{
+
+                const {_id,contraseña, __v,...resto}=ususario._doc;
+                res.json(resto);
+
+            }
+        });
+    }else{
+        res.json({message:"contraseña incorrecta"});
+    }
 
 };
 
--- backend/src/controllers/login.js
+++ backend/src/controllers/login.js
@@ -1,4 +1,24 @@
+const bycrypt=require("bcrypt");
+const usario=require("../models/usuario")
+const User=require("../models/usuario");
 const login=async (req,resp)=>{
+    const{coorreo,contraseña}=req.body;
+    if(!usuario){
+        return res.json({message: "usario no encontrado"})
+    }
+    bycrypt.compare(contraseña,usuario.contraseña).then((correcta)=>{
+        if(correcta){
+            const {id}=usario;
+            res.json({
+                message:"se ha Registrado correctamente",
+                usuario: {
+                    id,
+                }
+            });
+        }else{
+            return res.json({message:"su contraseña es incorrecta"});
+        }
+    })
 
 };
 
